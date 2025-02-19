42ceae3b495061f089ffb418af3c82f9fd8046fb
name <karankarandevgan@gmail.com>
2025-02-19 11:53:35 +0530

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const { resolve } = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+//const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+//app.use(express.static('static'));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB database"))
+  .catch((error) =>
+    console.log("Error connecting to database: " + error.message)
+  );
+app.get("/", (req, res) => {
+  // res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.send("Customer Management System Backend is Running!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
